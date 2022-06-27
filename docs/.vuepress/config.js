85187ff4af3a87d50681f31540cf7d07ad932cff
纯天然绿色学渣 <7474841+bc160@user.noreply.gitee.com>
2022-06-27 15:49:43 +0800

    add kanbanniang

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,5 +17,20 @@ module.exports = {
             apiKey: '<API_KEY>',
             indexName: '<INDEX_NAME>'
           }
-    }
+    },
+    plugins:[
+        // 插件的配置
+        ['@vuepress/back-to-top'],
+        ['@vuepress-reco/vuepress-plugin-kan-ban-niang',{
+            theme: ["z16"],
+            clean: false,
+            info: 'https://github.com/mengqiuleo',
+            messages: {
+              welcome: '',
+              home: '心里的花，我想要带你回家',
+              theme: '好吧，希望你能喜欢我的其他小伙伴。',
+              close: '再见哦'
+            }
+          }]
+    ]
 }
